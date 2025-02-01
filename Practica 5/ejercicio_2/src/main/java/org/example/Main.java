1459de0d9f43c360045df38a361f59e7e983100b
JJCAMPOSM <josecamposm0d@gmail.com>
2025-01-31 20:10:14 -0600

    Ejercicio 2 Terminado

--- Practica 5/ejercicio_2/src/main/java/org/example/Main.java
+++ Practica 5/ejercicio_2/src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Ingrese el un numero positivo entero: ");
+        System.out.println("Ingrese el un numero positivo entero : ");
         int num = sc.nextInt();
 
         for(int i = num; i >= 0; i--){
