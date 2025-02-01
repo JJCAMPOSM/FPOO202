0c7baa024d3efddfbfd75a4577eddc2fdd1006d1
JJCAMPOSM <josecamposm0d@gmail.com>
2025-01-31 20:16:46 -0600

    Ejercicio 3 Terminado

--- Practica 5/ejercicio_3/src/main/java/org/example/Main.java
+++ Practica 5/ejercicio_3/src/main/java/org/example/Main.java
@@ -1,11 +1,11 @@
 package org.example;
 
 public class Main {
+
     public static void main(String[] args) {
-        for (int i = 0; i == 10; i++) {
-            for (int j = 0; j == 10; j++) {
-                System.out.print(i + "x" + j + " = " + i * j);
-                System.out.println();
+        for (int i = 0; i <= 10; i++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.println(i + "x" + j + " = " + i * j);
             }
         }
     }
