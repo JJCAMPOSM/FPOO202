12af7555844df17d712d55b96a00ed960e2541aa
JJCAMPOSM <josecamposm0d@gmail.com>
2025-03-11 14:56:23 -0600

    Examen 2 Terminado

--- Examen2doP/Examen_2/src/Alumno.java
+++ Examen2doP/Examen_2/src/Alumno.java
@@ -6,29 +6,37 @@ public class Alumno {
     private String apellidoMaterno;
     private int anioNacimiento;
     private String carrera;
+    private int anioCurso;
 
-    Alumno(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, String carrera) {
+    Alumno(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, String carrera, int anioCurso) {
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
         this.anioNacimiento = anioNacimiento;
         this.carrera = carrera;
+        this.anioCurso = anioCurso;
     }
 
-    public void registrarAlumno(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, String carrera) {
-    this.nombre = JOptionPane.showInputDialog("Ingrese el nombre del alumno");
-    this.apellidoPaterno = JOptionPane.showInputDialog("Ingrese el apellido paterno del alumno");
-    this.apellidoMaterno = JOptionPane.showInputDialog("Ingrese el apellido materno del alumno");
-    this.anioNacimiento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de nacimiento del alumno"));
-    this.carrera = JOptionPane.showInputDialog("Ingrese la carrera del alumno");
+    public void registrarAlumno(String nombre, String apellidoPaterno, String apellidoMaterno, int anioNacimiento, String carrera, int anioCurso) {
+        this.nombre = JOptionPane.showInputDialog("Ingrese el nombre del alumno");
+        this.apellidoPaterno = JOptionPane.showInputDialog("Ingrese el apellido paterno del alumno");
+        this.apellidoMaterno = JOptionPane.showInputDialog("Ingrese el apellido materno del alumno");
+        this.anioNacimiento = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año de nacimiento del alumno"));
+        this.carrera = JOptionPane.showInputDialog("Ingrese la carrera del alumno");
+        this.anioCurso = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el año en curso: "));
     }
     public String generarMatricula() {
         String matricula = "";
-        matricula += nombre.substring(0, 2);
-        matricula += apellidoPaterno.substring(0, 2);
-        matricula += apellidoMaterno.substring(0, 2);
-        matricula += String.valueOf(anioNacimiento).substring(0, 2);
-        matricula += carrera.substring(0, 2);
+        String numeros= "0123456789";
+        matricula += String.valueOf(anioCurso).substring(2, 4);
+        matricula += String.valueOf(anioNacimiento).substring(2, 4);
+        matricula += nombre.substring(0, 1);
+        matricula += apellidoPaterno.substring(0, 1);
+        matricula += apellidoMaterno.substring(0, 1);
+        matricula += numeros.charAt((int) (Math.random() * 10));
+        matricula += numeros.charAt((int) (Math.random() * 10));
+        matricula += numeros.charAt((int) (Math.random() * 10));
+        matricula += carrera.substring(0, 3);
         return matricula;
             }
         }
--- Examen2doP/Examen_2/src/Main.java
+++ Examen2doP/Examen_2/src/Main.java
@@ -1,8 +1,10 @@
+import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        Alumno alumno1 = new Alumno("", "", "", 0, "");
+        Alumno alumno1 = new Alumno("", "", "", 0, "", 0);
 
-        alumno1.registrarAlumno("", "", "", 0, "");
+        alumno1.registrarAlumno("", "", "", 0, "", 0);
+        JOptionPane.showMessageDialog(null, alumno1.generarMatricula());
     }
 }
